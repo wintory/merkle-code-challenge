39bd998537f41bb4202a07f6e397ef270913e9a3
wintory <thanapol.sakul@gmail.com>
2024-10-05 22:43:07 +0700

    fix: fix style

--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -2,7 +2,7 @@ import { SymbolStatus } from '../constants/symbol'
 
 const Card = ({ key, id, status }) => {
   return (
-    <div className="rounded-md bg-transparent p-4 text-left outline-double">
+    <div className="rounded-md bg-transparent p-4 text-left outline-double hover:bg-slate-800">
       <p>Symbol: {key}</p>
       <p>ID: {id}</p>
       <p>
