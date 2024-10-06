188e250fff3324cc123d53e94205e17427b59ed3
wintory <thanapol.sakul@gmail.com>
2024-10-06 16:54:22 +0700

    test: add more test

---
+++ backend/src/services/binanceService.test.js
@@ -0,0 +1,51 @@
+import axios from 'axios'
+import { getBinanceSymbolPrice } from './binanceService'
+
+describe('getBinanceSymbolPrice', () => {
+  const mockedAxios = jest.spyOn(axios, 'get')
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  beforeAll(() => {
+    process.env.BINANCE_API_URL = 'https://api.binance.com'
+  })
+
+  it('should return the correct price when API call is successful', async () => {
+    const mockResponse = {
+      data: {
+        price: '50000',
+      },
+    }
+    const symbol = 'BTCUSDT'
+    mockedAxios.mockResolvedValue(mockResponse)
+
+    const result = await getBinanceSymbolPrice({ symbol })
+
+    expect(mockedAxios).toHaveBeenCalledWith(
+      `https://api.binance.com/api/v3/ticker/price?symbol=${symbol}`
+    )
+    expect(result).toEqual(50000)
+  })
+
+  it('should throw an error when API call fails', async () => {
+    const symbol = 'BTC-USDT'
+    const mockError = new Error('API call failed')
+    mockedAxios.mockRejectedValue(mockError)
+
+    await expect(getBinanceSymbolPrice({ symbol })).rejects.toThrow(
+      'Failed to fetch price from Binance'
+    )
+  })
+
+  it('should throw an error when price data is invalid', async () => {
+    const symbol = 'BTC-USDT'
+    const mockResponse = {}
+    mockedAxios.mockResolvedValue(mockResponse)
+
+    await expect(getBinanceSymbolPrice({ symbol })).rejects.toThrow(
+      'Failed to fetch price from Binance'
+    )
+  })
+})
---
+++ backend/src/services/coinbaseService.test.js
@@ -0,0 +1,59 @@
+import axios from 'axios'
+import { getCoinbaseSymbolPrice } from './coinbaseService'
+
+describe('getCoinbaseSymbolPrice', () => {
+  const mockedAxios = jest.spyOn(axios, 'get')
+
+  afterEach(() => {
+    jest.clearAllMocks()
+  })
+
+  beforeAll(() => {
+    process.env.COINBASE_API_URL = 'https://api.coinbase.com'
+  })
+
+  it('should return the correct price when API call is successful', async () => {
+    const mockResponse = {
+      data: {
+        data: {
+          amount: '50000',
+        },
+      },
+    }
+    const symbol = 'BTC-USDT'
+    mockedAxios.mockResolvedValue(mockResponse)
+
+    const result = await getCoinbaseSymbolPrice({ symbol })
+
+    expect(mockedAxios).toHaveBeenCalledWith(
+      'https://api.coinbase.com/v2/prices/BTC-USDT/spot'
+    )
+    expect(result).toEqual(50000)
+  })
+
+  it('should throw an error when API call fails', async () => {
+    const symbol = 'BTC-USDT'
+    const mockError = new Error('API call failed')
+    mockedAxios.mockRejectedValue(mockError)
+
+    await expect(getCoinbaseSymbolPrice({ symbol })).rejects.toThrow(
+      'Failed to fetch price from Coinbase'
+    )
+  })
+
+  it('should throw an error when price data is invalid', async () => {
+    const symbol = 'BTC-USDT'
+    const mockResponse = {
+      data: {
+        data: {
+          amount: 'invalid',
+        },
+      },
+    }
+    mockedAxios.mockResolvedValue(mockResponse)
+
+    await expect(getCoinbaseSymbolPrice({ symbol })).rejects.toThrow(
+      'Failed to fetch price from Coinbase'
+    )
+  })
+})
